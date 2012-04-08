debbbe22bf8670abfbd350cf2e3cb709ffdcb26c
Alex Mishyn <mishyn@gmail.com>
2012-04-08 09:46:11 +0300

    fixed merge issue

--- lib/localized_url_for.rb
+++ lib/localized_url_for.rb
@@ -3,7 +3,9 @@ require 'rails'
 module LocalizedUrlFor
 
   def url_for_with_locale(options)
-    url_for_without_locale(options.merge(:locale => I18n.locale))
+    localized_options = options
+    localized_options = options.merge(:locale => I18n.locale) if options.respond_to?(:merge)
+    url_for_without_locale(localized_options)
   end
 
 end
--- lib/localized_url_for/version.rb
+++ lib/localized_url_for/version.rb
@@ -1,3 +1,3 @@
 module LocalizedUrlFor
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
